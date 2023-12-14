79eb9888ce93829f53e4d36bffcc253e6d7feeee
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-14 14:21:35 -0300

    chore(project): :heavy_minus_sign: Remove flowbite dependency and replace with default tailwindcss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [require('flowbite/plugin')],
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/container-queries')],
+	...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
 };
