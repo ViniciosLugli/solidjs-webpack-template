f6be66c82ff8c632ecd01fad8eceb814abb00f80
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-15 22:28:06 -0300

    feat(toast): :sparkles: Added toast lib to use

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import { render } from 'solid-js/web';
 import '@styles/index.scss';
+import toast, { Toaster } from 'solid-toast';
+
+const notify = () => toast.success('Here is your toast.');
 
 function HelloWorld() {
 	return (
-		<div class="text-3xl font-bold underline text-center">
-			<p>Hello World!</p>
-			<img src="image.svg" alt="test image" />
-		</div>
+		<>
+			<Toaster />
+			<div class="flex text-3xl font-bold underline text-center justify-center items-center h-screen">
+				<button onClick={notify}>Make me a toast</button>
+
+				<img src="image.svg" alt="test image" width="255" height="255" />
+			</div>
+		</>
 	);
 }
 
