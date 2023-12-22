dd50d7b6681114be8f47bf6cd1a0a7e8aae4566e
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-22 20:48:29 -0300

    feat(components): :sparkles: Added ark-ui components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import '@styles/index.scss';
 import toast, { Toaster } from 'solid-toast';
 import ExampleImage from '@components/image';
+import { Tooltip } from '@ark-ui/solid';
+import { Portal } from 'solid-js/web';
+
 const notify = () => toast.success('Here is your toast.');
 
 const Index = () => {
 	return (
 		<>
 			<Toaster />
-			<div class="flex text-3xl font-bold underline text-center justify-center items-center h-screen">
-				<button onClick={notify}>Make me a toast</button>
+			<div class="flex text-3xl text-center justify-center items-center h-screen">
+				<Tooltip.Root openDelay={250} closeDelay={250}>
+					<Tooltip.Trigger>
+						<button class="font-bold underline" onClick={notify}>
+							Make me a toast
+						</button>
+					</Tooltip.Trigger>
+					<Portal>
+						<Tooltip.Positioner>
+							<Tooltip.Content>Congrats, you made a toast!</Tooltip.Content>
+						</Tooltip.Positioner>
+					</Portal>
+				</Tooltip.Root>
 				<ExampleImage />
 			</div>
 		</>
