41cb6dd9d46f728fe56a84ed0596891d7230ab86
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-22 20:37:24 -0300

    feat(metadata): :sparkles: Added solidjs meta for SEO

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,17 @@
 import { render } from 'solid-js/web';
 import Index from '@pages/index';
+import { MetaProvider, Title, Link, Meta } from '@solidjs/meta';
 
-render(() => <Index />, document.querySelector('#app')!);
+render(
+	() => (
+		<MetaProvider>
+			<div class="Home">
+				<Title>Title of page</Title>
+				<Link rel="canonical" href="http://solidjs.com/" />
+				<Meta name="example" content="whatever" />
+				<Index />
+			</div>
+		</MetaProvider>
+	),
+	document.querySelector('#app')!
+);
